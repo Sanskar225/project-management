56da858da45be199db2b093243ae947d8407a958
sanskar225 <sanskarsinha225@gmail.com>
2025-11-03 01:34:24 +0530

    header index

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-
 type Props = {
   name: string;
   buttonComponent?: any;
   isSmallText?: boolean;
 };
-
 const Header = ({ name, buttonComponent, isSmallText = false }: Props) => {
   return (
     <div className="mb-5 flex w-full items-center justify-between">
       <h1
-        className={`${isSmallText ? "text-lg" : "text-2xl"} font-semibold dark:text-white`}
+        className={`${isSmallText ? "text-lg": "text-2xl"} font-semibold dark:text-white`}
       >
         {name}
       </h1>
@@ -18,5 +16,4 @@ const Header = ({ name, buttonComponent, isSmallText = false }: Props) => {
     </div>
   );
 };
-
 export default Header;
