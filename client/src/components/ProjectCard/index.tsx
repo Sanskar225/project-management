ad95efd28e2883b69db5c677a5bda729021a6331
sanskar225 <sanskarsinha225@gmail.com>
2025-11-03 01:43:48 +0530

    project card index

--- client/src/components/ProjectCard/index.tsx
+++ client/src/components/ProjectCard/index.tsx
@@ -10,7 +10,7 @@ const ProjectCard = ({ project }: Props) => {
     <div className="rounded border p-4 shadow">
       <h3>{project.name}</h3>
       <p>{project.description}</p>
-      <p>Start Date: {project.startDate}</p>
+      <p>Starting Date: {project.startDate}</p>
       <p>End Date: {project.endDate}</p>
     </div>
   );
