2d1191dc22c1ef48519abd7a39c05d9c384681db
sanskar225 <sanskarsinha225@gmail.com>
2025-11-03 01:36:06 +0530

    model index

--- client/src/components/Modal/index.tsx
+++ client/src/components/Modal/index.tsx
@@ -2,17 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "../Header";
 import { X } from "lucide-react";
-
 type Props = {
   children: React.ReactNode;
   isOpen: boolean;
   onClose: () => void;
   name: string;
 };
-
 const Modal = ({ children, isOpen, onClose, name }: Props) => {
   if (!isOpen) return null;
-
   return ReactDOM.createPortal(
     <div className="fixed inset-0 z-50 flex h-full w-full items-center justify-center overflow-y-auto bg-gray-600 bg-opacity-50 p-4">
       <div className="w-full max-w-2xl rounded-lg bg-white p-4 shadow-lg dark:bg-dark-secondary">
@@ -34,5 +31,4 @@ const Modal = ({ children, isOpen, onClose, name }: Props) => {
     document.body,
   );
 };
-
 export default Modal;
